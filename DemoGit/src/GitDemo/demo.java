8e24e92c568d494c320312d4fe031998c0a21b81
HP <HP@DESKTOP-N578P02.lan>
2024-12-11 17:49:45 +0530

    created me

--- DemoGit/src/GitDemo/demo.java
+++ DemoGit/src/GitDemo/demo.java
@@ -1,9 +1,13 @@
 package GitDemo;
 
 public class demo {
+	public void m1() {
+		System.out.println("m1 called");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		
 	}
 
 }
