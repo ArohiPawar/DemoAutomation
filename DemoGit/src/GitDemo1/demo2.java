a9c4374231b551d9369fa018dab29a15ee81b067
HP <HP@DESKTOP-N578P02.lan>
2024-12-11 18:53:47 +0530

    demo2 created

---
+++ DemoGit/src/GitDemo1/demo2.java
@@ -0,0 +1,12 @@
+package GitDemo1;
+
+public class demo2 {
+public void m1() {
+	System.out.println("hello m1");
+}
+	public static void main(String[] args) {
+		System.out.println("hello");
+
+	}
+
+}
