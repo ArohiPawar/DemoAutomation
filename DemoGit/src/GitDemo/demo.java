9801915d7449e8ffc93ebaf9ca8d145508f66b9f
ArohiPawar <99589611+ArohiPawar@users.noreply.github.com>
2024-12-11 18:50:52 +0530

    m3 created

--- DemoGit/src/GitDemo/demo.java
+++ DemoGit/src/GitDemo/demo.java
@@ -4,8 +4,11 @@ public class demo {
 	public void m1() {
 		System.out.println("m1 called");
 	}
-
-	public static void main(String[] args) {
+       public void m3()
+	{
+		System.out.println("m3");
+	}
+		public static void main(String[] args) {
 		System.out.println("Hello World");
 		
 	}
