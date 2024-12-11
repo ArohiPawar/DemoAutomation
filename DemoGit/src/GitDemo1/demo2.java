b6de400e6f4db17c27e5e45c66761a375c86f4b0
ArohiPawar <99589611+ArohiPawar@users.noreply.github.com>
2024-12-11 19:00:48 +0530

    created by me m2

--- DemoGit/src/GitDemo1/demo2.java
+++ DemoGit/src/GitDemo1/demo2.java
@@ -4,6 +4,10 @@ public class demo2 {
 public void m1() {
 	System.out.println("hello m1");
 }
+
+	public void m2(){
+		
+	}
 	public static void main(String[] args) {
 		System.out.println("hello");
 
